66f69a789a2675172a80277d9226f0dc381b66a2
Fraser Chang <shuhan0628@gmail.com>
2023-07-27 21:07:48 +0800

    Initial Commit

---
+++ MovieGesture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieGesture
+//
+//  Created by 張丰澤 on 2023/7/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
